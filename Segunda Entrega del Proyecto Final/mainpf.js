fd04a2c31789b85ad83cc5f57e9f5281871e436c
Diego Massara <massaradiego11@gmail.com>
2021-11-10 23:56:29 -0300

    Clase 12

--- Segunda Entrega del Proyecto Final/mainpf.js
+++ Segunda Entrega del Proyecto Final/mainpf.js
@@ -1,84 +1,28 @@
-const productos = [
-    {
-        id: 1,
-        nombre: 'Charmander - 46/102 - Common 1st Edition',
-        precio: 96.99,
-        imagen: 'charmander.jpg'
-    },
-    {
-        id: 2,
-        nombre: 'Squirtle - 63/102 - Common 1st Edition',
-        precio: 112.99,
-        imagen: 'squirtle.jpg'
-    },
-    {
-        id: 3,
-        nombre: 'Bulbasaur - 44/102 - Common 1st Edition',
-        precio: 439.99,
-        imagen: 'bulbasaur.jpg'
-    },
-    {
-        id: 4,
-        nombre: 'Pikachu - 58/102 - Common 1st Edition',
-        precio: 179.99,
-        imagen: 'pikachu.jpg'
-    },
-    {
-        id: 5,
-        nombre: 'Eevee - 51/64 - Common 1st Edition',
-        precio: 7.99,
-        imagen: 'eevee.jpg'
-    },
-    {
-        id: 6,
-        nombre: 'Meowth - 56/64 - Common 1st Edition',
-        precio: 1.48,
-        imagen: 'meowth.jpg'
-    }
-    
-
-];
+import {productos} from './productos.js';
 
 let carrito = [];
 let total = 0;
-const htmlItems = document.getElementById('items');
-const elementoCarrito = document.getElementById('carrito');
-const htmlTotal = document.getElementById('total');
-const htmlBotonVaciar = document.getElementById('vaciar');
-const htmlBotonComprar = document.getElementById('comprar');
 const guardar = window.localStorage;
 
-function htmlProductos() {
-    productos.forEach((dato) => {
-        const div = document.createElement('div');
-        div.classList.add('card', 'col-sm-4');
-        const body = document.createElement('div');
-        body.classList.add('card-body');
-        const title = document.createElement('h5');
-        title.classList.add('card-title');
-        title.textContent = dato.nombre;
-        const img = document.createElement('img');
-        img.classList.add('img-fluid');
-        img.setAttribute('src', dato.imagen);
-        const precio = document.createElement('p');
-        precio.classList.add('card-text');
-        precio.textContent = '$' + dato.precio;
-        const button = document.createElement('button');
-        button.classList.add('btn', 'btn-primary');
-        button.textContent = '+';
-        button.setAttribute('marcador', dato.id);
-        button.addEventListener('click', agregarProducto);
-        body.appendChild(img);
-        body.appendChild(title);
-        body.appendChild(precio);
-        body.appendChild(button);
-        div.appendChild(body);
-        htmlItems.appendChild(div);
-    });
+const htmlProductos = () => {
+for (const producto of productos) {
+    $(`#items`).append(`
+        <div class="card col-sm-4">
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <img src="${producto.imagen}" class="img-fluid" alt="${producto.nombre}">
+                <p class="card-text"> $${producto.precio}</p>
+                <button class="btn btn-primary" id="btn-add">Agregar al carrito</button>
+            </div>
+        </div>
+    `);
+    $(`#btn-add`).attr('marcador', producto.id);
+    //$(`#btn-add`).click(agregarProducto()); //No puedo hacerlo funcionar con jQuery
+}
 }
 
-function htmlCarrito() {
-    elementoCarrito.textContent = '';
+const renderCarrito = () => {
+    $('#carrito').empty();
     const nuevoCarrito = [...new Set(carrito)];
     nuevoCarrito.forEach((item) => {
         const articulo = productos.filter((artProductos) => {
@@ -87,26 +31,24 @@ function htmlCarrito() {
         const cantidad = carrito.reduce((total, itemId) => {
             return itemId === item ? total += 1 : total;
         }, 0);
-        const htmlArt = document.createElement('li');
-        htmlArt.classList.add('list-group-item', 'text-center', 'mx-6');
-        htmlArt.textContent = `${cantidad} x ${articulo[0].nombre} : $${articulo[0].precio}`;
-        const htmlButton = document.createElement('div');
-        const reducir = document.createElement('button');
-        reducir.classList.add('btn', 'btn-danger', 'mx-3');
-        reducir.textContent = '-';
-        reducir.dataset.item = item;
-        reducir.addEventListener('click', reducirCantidad);
-        const borrar = document.createElement('button');
-        borrar.classList.add('btn', 'btn-danger', 'mx-3');
-        borrar.textContent = 'X';
-        borrar.dataset.item = item;
-        borrar.addEventListener('click', borrarProd);
-        htmlButton.appendChild(borrar);
-        htmlButton.appendChild(reducir);
-        htmlArt.appendChild(htmlButton);
-        elementoCarrito.appendChild(htmlArt);
-    });
-}
+        $(`#carrito`).append(`
+            <li class="list-group-item text-center mx-6">
+                <span class="badge badge-primary">${cantidad}</span>
+                ${articulo[0].nombre}
+                <span class="badge badge-primary">$${articulo[0].precio}</span>
+            </li>
+            <div>
+                <button class="btn btn-danger mx-3" id="btn-eliminar")">Eliminar</button>
+            </div>
+            
+        `);
+        //const btnEliminar = document.getElementById('btn-eliminar');
+        //btnEliminar.dataset.item = item;
+        $(`#total`).calcularTotal();
+        $(`#btn-eliminar`).click(borrarProd());
+    }
+    );  
+}  
 
 function calcularTotal() {
     total = 0;
@@ -116,7 +58,7 @@ function calcularTotal() {
         });
         total = total + valor[0].precio;
     });
-    htmlTotal.textContent = total.toFixed(2);
+    $(`#total`).textContent = total.toFixed(2);
 }
 
 function guardarCarrito () {
@@ -130,9 +72,9 @@ function cargarCarrito () {
 }
 
 function agregarProducto(prod) {
-    carrito.push(prod.target.getAttribute('marcador'))
+    carrito.push(prod.target.getAttribute('marcador'));
     calcularTotal();
-    htmlCarrito();
+    renderCarrito();
     guardarCarrito();
 }
 
@@ -145,37 +87,21 @@ function borrarProd(prod) {
     carrito = carrito.filter((carritoId) => {
         return carritoId !== id;
     });
-    htmlCarrito();
+    renderCarrito();
     calcularTotal();
     guardarCarrito();
 }
-
-function reducirCantidad(item){
-    var consulta = confirm("Desea reducir la cantidad del producto?");
-    if (consulta == false){
-        return
-    }
-    carrito = carrito.reduce((total, itemId) => {
-        if(itemId === item){
-            total = total -= 1;
-        }
-    }, 0);
-    htmlCarrito();
-    calcularTotal();
-    guardarCarrito();
-}
-
 
 
 function vaciarCarrito() {
     carrito = [];
-    htmlCarrito();
+    renderCarrito();
     calcularTotal();
     localStorage.clear();
 
 }
 
-htmlBotonVaciar.addEventListener('click', vaciarCarrito);
+$(`#vaciar`).click(vaciarCarrito);
 
 function comprar(){
     var result = confirm("Confirma la compra?");
@@ -185,15 +111,14 @@ function comprar(){
     vaciarCarrito();
 }
 
-htmlBotonComprar.addEventListener('click', comprar);
+$(`#comprar`).click(comprar);
 
 
 cargarCarrito();
 htmlProductos();
 calcularTotal();
-htmlCarrito();
+renderCarrito();
 
-const boton = document.getElementById('boton');
 const input = document.getElementById('input');
 
 function buscar(texto) {
@@ -206,6 +131,6 @@ function buscar(texto) {
 }
 
 
-boton.addEventListener('click', buscar);
+$(`#boton`).click(buscar());
 
 
---
+++ Segunda Entrega del Proyecto Final/productos.js
@@ -0,0 +1,46 @@
+const productos = [
+    {
+        id: 1,
+        nombre: 'Charmander - 46/102 - Common 1st Edition',
+        precio: 96.99,
+        imagen: 'charmander.jpg'
+    },
+    {
+        id: 2,
+        nombre: 'Squirtle - 63/102 - Common 1st Edition',
+        precio: 112.99,
+        imagen: 'squirtle.jpg'
+    },
+    {
+        id: 3,
+        nombre: 'Bulbasaur - 44/102 - Common 1st Edition',
+        precio: 439.99,
+        imagen: 'bulbasaur.jpg'
+    },
+    {
+        id: 4,
+        nombre: 'Pikachu - 58/102 - Common 1st Edition',
+        precio: 179.99,
+        imagen: 'pikachu.jpg'
+    },
+    {
+        id: 5,
+        nombre: 'Eevee - 51/64 - Common 1st Edition',
+        precio: 7.99,
+        imagen: 'eevee.jpg'
+    },
+    {
+        id: 6,
+        nombre: 'Meowth - 56/64 - Common 1st Edition',
+        precio: 1.48,
+        imagen: 'meowth.jpg'
+    }
+    
+
+];
+
+export {
+    
+    productos
+
+};
